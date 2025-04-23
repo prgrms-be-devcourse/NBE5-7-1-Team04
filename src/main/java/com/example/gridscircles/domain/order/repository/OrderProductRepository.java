2fe495284cca3365be5a32383038512fa6357508
pbk2312 <pbk2312@inu.ac.kr>
2025-04-23 14:41:30 +0900

    :sparkles: feat: 상품 정보와 함께 주문 상품 조회 메서드 작성

--- src/main/java/com/example/gridscircles/domain/order/repository/OrderProductRepository.java
+++ src/main/java/com/example/gridscircles/domain/order/repository/OrderProductRepository.java
@@ -1,9 +1,18 @@
 package com.example.gridscircles.domain.order.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.gridscircles.domain.order.entity.OrderProduct;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderProductRepository extends JpaRepository<OrderProduct, Long> {
 
+    @Query("""
+        select op
+          from OrderProduct op
+          join fetch op.product p
+         where op.orders.id = :orderId
+        """)
+    List<OrderProduct> findByOrdersIdWithProduct(@Param("orderId") Long orderId);
 }
