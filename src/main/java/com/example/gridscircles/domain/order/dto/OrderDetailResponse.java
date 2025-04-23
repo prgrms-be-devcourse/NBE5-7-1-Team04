f2d466e3974750da4f652099d63b33b891a216c2
pbk2312 <pbk2312@inu.ac.kr>
2025-04-23 14:43:33 +0900

    :sparkles: feat: 주문 상세 정보를 담는 DTO 생성

---
+++ src/main/java/com/example/gridscircles/domain/order/dto/OrderDetailResponse.java
@@ -0,0 +1,32 @@
+package com.example.gridscircles.domain.order.dto;
+
+import com.example.gridscircles.domain.order.enums.OrderStatus;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderDetailResponse {
+
+    private List<OrderProductDetailResponse> orderProducts;
+    private Integer totalQuantity;
+    private Integer totalPrice;
+    private String address;
+    private String zipcode;
+    private OrderStatus orderStatus;
+
+    @Builder
+    public OrderDetailResponse(List<OrderProductDetailResponse> orderProducts,
+        Integer totalQuantity,
+        Integer totalPrice, String address, String zipcode, OrderStatus orderStatus) {
+        this.orderProducts = orderProducts;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.orderStatus = orderStatus;
+    }
+}
