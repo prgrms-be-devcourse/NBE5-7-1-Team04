5bac8f96b235b2460b734f42e2857bb9be6e9bcc
pbk2312 <pbk2312@inu.ac.kr>
2025-04-23 16:59:09 +0900

    :wrench: fix: OrderService에 있는 `@Transactional(readOnly = true)` 제거

--- src/main/java/com/example/gridscircles/domain/order/service/OrderService.java
+++ src/main/java/com/example/gridscircles/domain/order/service/OrderService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class OrderService {
 
     private final OrderProductRepository orderProductRepository;
