9eae8575332429c89ec17ed407b2d960a2d38623
김건우 <rjswjddn@gmail.com>
2025-04-24 15:27:47 +0900

    :sparkles: feat: 주문 생성 후, 이동 페이지 수정

--- src/main/java/com/example/gridscircles/domain/order/controller/OrdersController.java
+++ src/main/java/com/example/gridscircles/domain/order/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.example.gridscircles.domain.order.controller;
 
 import com.example.gridscircles.domain.order.dto.CreateOrdersDto;
+import com.example.gridscircles.domain.order.dto.CreateOrdersResponseDto;
 import com.example.gridscircles.domain.order.dto.EmailDto;
 import com.example.gridscircles.domain.order.dto.OrderDetailDto;
 import com.example.gridscircles.domain.order.entity.Orders;
@@ -8,21 +9,21 @@ import com.example.gridscircles.domain.order.service.OrdersService;
 import com.example.gridscircles.domain.product.dto.ProductDto;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/orders")
@@ -91,9 +92,9 @@ public class OrdersController {
 
     @ResponseBody
     @PostMapping("")
-    public ResponseEntity<Long> saveOrders(
+    public ResponseEntity<CreateOrdersResponseDto> saveOrders(
         @Validated @RequestBody CreateOrdersDto createOrdersDto) {
         Long ordersId = ordersService.saveOrders(createOrdersDto);
-        return ResponseEntity.ok(ordersId);
+        return ResponseEntity.ok(new CreateOrdersResponseDto(ordersId));
     }
 }
---
+++ src/main/java/com/example/gridscircles/domain/order/dto/CreateOrdersResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.gridscircles.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateOrdersResponseDto {
+    private Long ordersId;
+}
