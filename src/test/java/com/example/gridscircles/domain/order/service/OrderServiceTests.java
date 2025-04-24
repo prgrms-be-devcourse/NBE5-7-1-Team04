9817fc0d560eb42435449993583a9d7b55d8b222
pbk2312 <pbk2312@inu.ac.kr>
2025-04-24 10:33:08 +0900

    :white_check_mark: test: 주문 수정 테스트 작성 및 주문 상세 조회 테스트 수정
    - 주문 상세 조회 이미지 경로 -> 바이트로 수정
    - 주문 수정 확인 테스트

--- src/test/java/com/example/gridscircles/domain/order/service/OrderServiceTests.java
+++ src/test/java/com/example/gridscircles/domain/order/service/OrderServiceTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.example.gridscircles.domain.order.dto.OrderDetailDto;
 import com.example.gridscircles.domain.order.dto.OrderProductDetailDto;
+import com.example.gridscircles.domain.order.dto.OrderUpdateDto;
 import com.example.gridscircles.domain.order.entity.OrderProduct;
 import com.example.gridscircles.domain.order.entity.Orders;
 import com.example.gridscircles.domain.order.enums.OrderStatus;
@@ -37,20 +38,24 @@ class OrderServiceTests {
     @Test
     @DisplayName("주문 상세 조회 테스트 - 여러 상품")
     void test_find_orderDetails_multipleProducts() throws Exception {
+
         // given: 두 개의 상품 생성 및 저장
+        byte[] dummyImage1 = "dummy-image-1".getBytes();
+        byte[] dummyImage2 = "dummy-image-2".getBytes();
+
         Product product1 = Product.builder()
             .name("사바하 커피1")
             .price(1000)
             .category(Category.DRINK)
             .description("맛있어요!")
-            .image("https://Yuhan.com/image1.jpg")
+            .image(dummyImage1)
             .build();
         Product product2 = Product.builder()
             .name("사바하 커피2")
             .price(1500)
             .category(Category.DRINK)
             .description("최고예요!")
-            .image("https://Yuhan.com/image2.jpg")
+            .image(dummyImage2)
             .build();
         productRepository.save(product1);
         productRepository.save(product2);
@@ -112,5 +117,34 @@ class OrderServiceTests {
         );
 
         assertThat(ex.getMessage()).isEqualTo("주문 정보를 조회할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("주문 수정 테스트")
+    void test_updateOrder() {
+
+        // given: 주문 생성
+        Orders order = Orders.builder()
+            .email("Yuhan@example.com")
+            .address("서울특별시 강남구 사바하아파트 444동 444호")
+            .zipcode("44444")
+            .totalPrice(0)
+            .orderStatus(OrderStatus.PROCESSING)
+            .build();
+        ordersRepository.save(order);
+
+        OrderUpdateDto updateDto = OrderUpdateDto.builder()
+            .address("서울특별시 용산구 사바하아파트 444동 444호")
+            .zipcode("12345")
+            .build();
+
+        // when
+        ordersService.updateOrder(order.getId(), updateDto);
+
+        // then
+        Orders updated = ordersRepository.findById(order.getId())
+            .orElseThrow();
+        assertThat(updated.getAddress()).isEqualTo("서울특별시 용산구 사바하아파트 444동 444호");
+        assertThat(updated.getZipcode()).isEqualTo("12345");
     }
 }
