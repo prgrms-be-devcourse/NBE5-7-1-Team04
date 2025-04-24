bbb9ec0ac54c7a1e5c3c2b8b9e35983b08d545c6
pbk2312 <pbk2312@inu.ac.kr>
2025-04-24 18:32:07 +0900

    :recycle: refactor: 주문 취소 Delete -> PUT으로 변경

--- src/main/java/com/example/gridscircles/domain/order/controller/OrdersController.java
+++ src/main/java/com/example/gridscircles/domain/order/controller/OrdersController.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -118,10 +117,10 @@ public class OrdersController {
         return ResponseEntity.noContent().build();
     }
 
-    @DeleteMapping("/{orderId}")
+    @PutMapping("/{orderId}/cancel")
     @ResponseBody
-    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
-        ordersService.deleteOrder(orderId);
+    public ResponseEntity<Void> cancelOrder(@PathVariable Long orderId) {
+        ordersService.cancelOrder(orderId);
         return ResponseEntity.noContent().build();
     }
 }
