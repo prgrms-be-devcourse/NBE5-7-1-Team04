92a7575412e5ded758edd8554a04db593a9c0222
pbk2312 <pbk2312@inu.ac.kr>
2025-04-25 22:33:19 +0900

    :sparkles: feat: 주문 수정 검증 validate 메서드 추가

--- src/main/java/com/example/gridscircles/domain/order/util/OrdersValidator.java
+++ src/main/java/com/example/gridscircles/domain/order/util/OrdersValidator.java
@@ -2,12 +2,23 @@ package com.example.gridscircles.domain.order.util;
 
 import com.example.gridscircles.domain.order.enums.OrderStatus;
 import com.example.gridscircles.domain.order.exception.OrderCancelException;
+import com.example.gridscircles.domain.order.exception.OrderUpdateException;
 
 public class OrdersValidator {
 
     public static void validateCancelable(OrderStatus status) {
-        if (status == OrderStatus.COMPLETED) {
-            throw new OrderCancelException("배송이 완료된 주문은 취소할 수 없습니다.");
+        if (isStatusCompletedOrCanceled(status)) {
+            throw new OrderCancelException("배송이 완료되거나 주문이 취소된 상태면 주문을 취소하실 수 없습니다.");
         }
+    }
+
+    public static void validateUpdateable(OrderStatus status) {
+        if (isStatusCompletedOrCanceled(status)) {
+            throw new OrderUpdateException("배송이 완료되거나 주문이 취소된 상태면 주문을 수정하실 수 없습니다.");
+        }
+    }
+
+    private static boolean isStatusCompletedOrCanceled(OrderStatus status) {
+        return status == OrderStatus.COMPLETED || status == OrderStatus.CANCELED;
     }
 }
