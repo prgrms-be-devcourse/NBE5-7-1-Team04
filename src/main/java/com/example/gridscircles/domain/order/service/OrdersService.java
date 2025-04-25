2a008ff7ce2123bb9bb48fd43f86e2a4eae8ef24
Yuhan Park <156402683+pbk2312@users.noreply.github.com>
2025-04-25 10:52:18 +0900

    [hotfix] : 사라진 주문 취소 로직 추가 (#25)
    * :sparkles: refactor: 수정
    
    * :rotating_light: fix: 취소 Service 추가

--- src/main/java/com/example/gridscircles/domain/order/service/OrdersService.java
+++ src/main/java/com/example/gridscircles/domain/order/service/OrdersService.java
@@ -31,136 +31,157 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrdersService {
 
 
-  private final OrdersRepository ordersRepository;
-  private final OrderProductRepository orderProductRepository;
-  private final ProductRepository productRepository;
+    private final OrdersRepository ordersRepository;
+    private final OrderProductRepository orderProductRepository;
+    private final ProductRepository productRepository;
 
-  // 특정 ID에 product와 order정보 조회
-  @Transactional(readOnly = true)
-  public OrdersSearchResponse searchOrderWithItems(Long orderId) {
-    Orders orders = ordersRepository.findById(orderId).orElseThrow(()
-        -> new NoSuchElementException("해당 주문은 존재 하지 않습니다. ID: " + orderId));
+    // 특정 ID에 product와 order정보 조회
+    @Transactional(readOnly = true)
+    public OrdersSearchResponse searchOrderWithItems(Long orderId) {
+        Orders orders = ordersRepository.findById(orderId).orElseThrow(()
+            -> new NoSuchElementException("해당 주문은 존재 하지 않습니다. ID: " + orderId));
 
-    List<ProductInfoResponse> products = orders.getOrderProducts().stream()
-        .map(orderProduct -> new ProductInfoResponse(
-            orderProduct.getProduct().getName(),
-            orderProduct.getQuantity(),
-            orderProduct.getPrice()
-        )).collect(Collectors.toList());
+        List<ProductInfoResponse> products = orders.getOrderProducts().stream()
+            .map(orderProduct -> new ProductInfoResponse(
+                orderProduct.getProduct().getName(),
+                orderProduct.getQuantity(),
+                orderProduct.getPrice()
+            )).collect(Collectors.toList());
 
-    return new OrdersSearchResponse(
-        orders.getId(),
-        orders.getEmail(),
-        orders.getAddress(),
-        orders.getZipcode(),
-        orders.getTotalPrice(),
-        orders.getOrderStatus(),
-        orders.getCreatedAt(),
-        products
-    );
+        return new OrdersSearchResponse(
+            orders.getId(),
+            orders.getEmail(),
+            orders.getAddress(),
+            orders.getZipcode(),
+            orders.getTotalPrice(),
+            orders.getOrderStatus(),
+            orders.getCreatedAt(),
+            products
+        );
 
-  }
+    }
 
-  // 전체 주문 조회
-  @Transactional(readOnly = true)
-  public Page<OrdersSearchResponse> getAllOrders(Pageable pageable) {
-    Page<Orders> ordersPage = ordersRepository.findAllByOrderByCreatedAtDesc(pageable);
+    // 전체 주문 조회
+    @Transactional(readOnly = true)
+    public Page<OrdersSearchResponse> getAllOrders(Pageable pageable) {
+        Page<Orders> ordersPage = ordersRepository.findAllByOrderByCreatedAtDesc(pageable);
 
-    return ordersPage.map(order -> new OrdersSearchResponse(
-        order.getId(),
-        order.getEmail(),
-        order.getAddress(),
-        order.getZipcode(),
-        order.getTotalPrice(),
-        order.getOrderStatus(),
-        order.getCreatedAt(),
-        order.getOrderProducts().stream()
-            .map(item -> new ProductInfoResponse(
-                item.getProduct().getName(),
-                item.getQuantity(),
-                item.getPrice()
-            ))
-            .collect(Collectors.toList())
-    ));
-
-
-  }
-
-  @Transactional(readOnly = true)
-  public OrderDetailResponse getOrderDetail(Long orderId) {
-    List<OrderProduct> products = orderProductRepository
-        .findByOrdersIdWithProductAndOrder(orderId);
-
-    Orders findOrder = products.stream()
-        .findFirst()
-        .map(OrderProduct::getOrders)
-        .orElseThrow(() -> new OrderNotFoundException("주문 정보를 조회할 수 없습니다."));
-
-    return OrdersMapper.toOrderDetailResponse(findOrder, products, calculateQuantity(products),
-        calculdateTotalPrice(products));
-  }
-
-  private static int calculdateTotalPrice(List<OrderProduct> products) {
-    return products.stream()
-        .mapToInt(op -> op.getPrice() * op.getQuantity())
-        .sum();
-  }
-
-  private static int calculateQuantity(List<OrderProduct> products) {
-    return products.stream()
-        .mapToInt(OrderProduct::getQuantity)
-        .sum();
-  }
-
-  @Transactional
-  public void updateOrder(Long orderId, OrderUpdateRequest orderUpdateRequest) {
-
-    Orders findOrder = ordersRepository.findById(orderId)
-        .orElseThrow(() -> new OrderNotFoundException("주문 정보를 찾을 수 없습니다."));
-
-    findOrder.updateOrder(orderUpdateRequest);
-
-    ordersRepository.save(findOrder);
-  }
+        return ordersPage.map(order -> new OrdersSearchResponse(
+            order.getId(),
+            order.getEmail(),
+            order.getAddress(),
+            order.getZipcode(),
+            order.getTotalPrice(),
+            order.getOrderStatus(),
+            order.getCreatedAt(),
+            order.getOrderProducts().stream()
+                .map(item -> new ProductInfoResponse(
+                    item.getProduct().getName(),
+                    item.getQuantity(),
+                    item.getPrice()
+                ))
+                .collect(Collectors.toList())
+        ));
 
 
-  @Transactional
-  public CreateOrdersResponse saveOrders(CreateOrdersRequest createOrdersRequest) {
-    Orders createOrders = OrdersMapper.fromCreateOrdersRequest(createOrdersRequest);
-    List<OrderProduct> orderProducts = createOrderProducts(createOrdersRequest.getProducts(), createOrders);
+    }
 
-    int totalPrice = orderProducts.stream()
-        .mapToInt(OrderProduct::getPrice)
-        .sum();
+    @Transactional(readOnly = true)
+    public OrderDetailResponse getOrderDetail(Long orderId) {
+        List<OrderProduct> products = orderProductRepository
+            .findByOrdersIdWithProductAndOrder(orderId);
 
-    createOrders.setTotalPrice(totalPrice);
-    ordersRepository.save(createOrders);
-    orderProductRepository.saveAll(orderProducts);
-    return OrdersMapper.toCreateOrdersResponse(createOrders);
-  }
+        Orders findOrder = products.stream()
+            .findFirst()
+            .map(OrderProduct::getOrders)
+            .orElseThrow(() -> new OrderNotFoundException("주문 정보를 조회할 수 없습니다."));
 
-  public Page<Orders> getOrdersByEmail(String email, Pageable pageable) {
-    return ordersRepository.findByEmailOrderByCreatedAtDesc(email, pageable);
-  }
+        return OrdersMapper.toOrderDetailResponse(findOrder, products, calculateQuantity(products),
+            calculdateTotalPrice(products));
+    }
 
-  public List<Orders> getOrderById(Long id, String email) {
-    return ordersRepository.findByIdAndEmailOrderByCreatedAt(id, email);
-  }
+    private static int calculdateTotalPrice(List<OrderProduct> products) {
+        return products.stream()
+            .mapToInt(op -> op.getPrice() * op.getQuantity())
+            .sum();
+    }
 
-  private List<OrderProduct> createOrderProducts(List<CreateOrdersProductDto> productsDto,
-      Orders order) {
-    return productsDto.stream()
-        .map(dto -> {
-          Product product = productRepository.findById(dto.getId())
-              .orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
-          return OrderProductMapper.fromCreateOrdersProductDto(dto, order, product);
-        })
-        .toList();
-  }
+    private static int calculateQuantity(List<OrderProduct> products) {
+        return products.stream()
+            .mapToInt(OrderProduct::getQuantity)
+            .sum();
+    }
 
-  @Transactional
-  public void completeOrders() {
-    ordersRepository.updateOrdersStatusByOrderStatus(OrderStatus.PROCESSING, OrderStatus.COMPLETED);
-  }
+    @Transactional
+    public void updateOrder(Long orderId, OrderUpdateRequest orderUpdateRequest) {
+
+        Orders findOrder = ordersRepository.findById(orderId)
+            .orElseThrow(() -> new OrderNotFoundException("주문 정보를 찾을 수 없습니다."));
+
+        findOrder.updateOrder(orderUpdateRequest);
+
+        ordersRepository.save(findOrder);
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
+        Orders findOrder = getOrderById(orderId);
+
+        validateOrderStatus(findOrder);
+        findOrder.cancel();
+    }
+
+    private static void validateOrderStatus(Orders findOrder) {
+        if (findOrder.getOrderStatus() == OrderStatus.COMPLETED) {
+            throw new OrderNotFoundException("배송이 완료된 주문은 취소할 수 없습니다.");
+        }
+    }
+
+    private Orders getOrderById(Long orderId) {
+        return ordersRepository.findById(orderId)
+            .orElseThrow(() -> new OrderNotFoundException("주문 정보를 찾을 수 없습니다."));
+    }
+    
+    @Transactional
+    public CreateOrdersResponse saveOrders(CreateOrdersRequest createOrdersRequest) {
+        Orders createOrders = OrdersMapper.fromCreateOrdersRequest(createOrdersRequest);
+        List<OrderProduct> orderProducts = createOrderProducts(createOrdersRequest.getProducts(),
+            createOrders);
+
+        int totalPrice = orderProducts.stream()
+            .mapToInt(OrderProduct::getPrice)
+            .sum();
+
+        createOrders.setTotalPrice(totalPrice);
+        ordersRepository.save(createOrders);
+        orderProductRepository.saveAll(orderProducts);
+        return OrdersMapper.toCreateOrdersResponse(createOrders);
+    }
+
+    public Page<Orders> getOrdersByEmail(String email, Pageable pageable) {
+        return ordersRepository.findByEmailOrderByCreatedAtDesc(email, pageable);
+    }
+
+    public List<Orders> getOrderById(Long id, String email) {
+        return ordersRepository.findByIdAndEmailOrderByCreatedAt(id, email);
+    }
+
+    private List<OrderProduct> createOrderProducts(List<CreateOrdersProductDto> productsDto,
+        Orders order) {
+        return productsDto.stream()
+            .map(dto -> {
+                Product product = productRepository.findById(dto.getId())
+                    .orElseThrow(() -> new NoSuchElementException("존재하지 않는 상품입니다."));
+                return OrderProductMapper.fromCreateOrdersProductDto(dto, order, product);
+            })
+            .toList();
+    }
+
+    @Transactional
+    public void completeOrders() {
+        ordersRepository.updateOrdersStatusByOrderStatus(OrderStatus.PROCESSING,
+            OrderStatus.COMPLETED);
+    }
 
 }
