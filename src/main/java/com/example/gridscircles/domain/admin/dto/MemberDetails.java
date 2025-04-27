1c946e876269ebb7242ff8bc56a37952a69e078e
pbk2312 <pbk2312@inu.ac.kr>
2025-04-27 11:37:32 +0900

    :sparkles: feat: UserDetails 구현용 MemberDetails 클래스 작성

---
+++ src/main/java/com/example/gridscircles/domain/admin/dto/MemberDetails.java
@@ -0,0 +1,57 @@
+package com.example.gridscircles.domain.admin.dto;
+
+import com.example.gridscircles.domain.admin.entitiy.Member;
+import com.example.gridscircles.domain.admin.enums.Role;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MemberDetails implements UserDetails {
+
+    private final String email;
+    private final String password;
+    private final Role role;
+
+    public MemberDetails(Member member) {
+        this.email = member.getEmail();
+        this.password = member.getPassword();
+        this.role = member.getRole();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
