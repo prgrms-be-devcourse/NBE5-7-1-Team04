77c1f1349655725f60e374b90142a4e6eee3ca25
hwangsehee <65377385+sehee123@users.noreply.github.com>
2025-04-28 15:00:40 +0900

    :recycle: refactor: 공통 예외처리 적용 (#80)

--- src/test/java/com/example/gridscircles/domain/product/service/ProductServiceTests.java
+++ src/test/java/com/example/gridscircles/domain/product/service/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.example.gridscircles.domain.product.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.example.gridscircles.domain.product.dto.ProductCreateRequest;
 import com.example.gridscircles.domain.product.dto.ProductResponse;
@@ -8,15 +9,19 @@ import com.example.gridscircles.domain.product.dto.ProductUpdateRequest;
 import com.example.gridscircles.domain.product.entity.Product;
 import com.example.gridscircles.domain.product.enums.Category;
 import com.example.gridscircles.domain.product.repository.ProductRepository;
+import com.example.gridscircles.global.exception.ErrorException;
+import java.io.IOException;
 import java.util.Base64;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Transactional
 @SpringBootTest
@@ -56,6 +61,30 @@ class ProductServiceTests {
     }
 
     @Test
+    @DisplayName("상품 저장 파일 오류 테스트 ")
+    void saveProductImageExceptionTest() throws Exception {
+
+        MultipartFile mockFile = Mockito.mock(MultipartFile.class);
+
+        Mockito.when(mockFile.getBytes()).thenThrow(new IOException("파일 읽기 실패"));
+
+        ProductCreateRequest productCreateRequest = ProductCreateRequest.builder()
+            .name("상품명")
+            .category(Category.DRINK)
+            .price("1,000")
+            .description("설명")
+            .file(mockFile)
+            .build();
+
+        assertThatThrownBy(
+            () -> {
+                productService.saveProduct(productCreateRequest);
+            }
+        ).isInstanceOf(ErrorException. class);
+
+    }
+
+    @Test
     @DisplayName("상품 상세 조회 테스트")
     void findByIdProductTest() throws Exception {
         MockMultipartFile mockFile =
@@ -82,6 +111,26 @@ class ProductServiceTests {
         assertThat(productResponse.getBase64EncodeImage()).isEqualTo(
             Base64.getEncoder().encodeToString(product.getImage()));
         assertThat(productResponse.getContentType()).isEqualTo(product.getContentType());
+    }
+
+    @Test
+    @DisplayName("상품 상세 조회 오류 테스트 ")
+    void findByIdProductExceptionTest() throws Exception {
+
+        Long maxId = productRepository.findAll()
+            .stream()
+            .mapToLong(Product::getId)
+            .max()
+            .orElse(0L);
+
+        Long nonExistentId = maxId + 1;
+
+        assertThatThrownBy(
+            () -> {
+                productService.findProductById(nonExistentId);
+            }
+        ).isInstanceOf(ErrorException. class);
+
     }
 
     @Test
@@ -221,5 +270,83 @@ class ProductServiceTests {
                 productUpdateRequest.getFile().getContentType());
             assertThat(updatedProduct.getDel_yn()).isEqualTo("N");
         }
+
+        @Test
+        @DisplayName("상품 수정 오류 테스트 ")
+        void updateProductExceptionTest () throws Exception {
+
+            MockMultipartFile emptyFile = new MockMultipartFile("file", "", "image/jpeg",
+                new byte[0]);
+
+            Long maxId = productRepository.findAll()
+                .stream()
+                .mapToLong(Product::getId)
+                .max()
+                .orElse(0L);
+
+            Long nonExistentId = maxId + 1;
+
+            ProductUpdateRequest productUpdateRequest = ProductUpdateRequest.builder()
+                .id(nonExistentId)
+                .name("상품명 수")
+                .description("상품설정 수정")
+                .price("2000")
+                .category(Category.BREAD)
+                .file(emptyFile)
+                .base64EncodeImage(Base64.getEncoder().encodeToString(emptyFile.getBytes()))
+                .contentType(emptyFile.getContentType())
+                .build();
+
+            assertThatThrownBy(
+                () -> {
+                    productService.updateProduct(productUpdateRequest);
+                }
+            ).isInstanceOf(ErrorException.class);
+
+
+        }
+
+        @Test
+        @DisplayName("상품 수정 오류 테스트 - 이미지 오류")
+        void updateProductImageExceptionTest () throws Exception {
+
+            MockMultipartFile mockFile =
+                new MockMultipartFile("image_name", "origin_image.jpg", "image/jpeg",
+                    "image.jpg".getBytes());
+
+            Product product = Product.builder()
+                .name("상품명")
+                .description("상품설명")
+                .price(1000)
+                .category(Category.DRINK)
+                .image(mockFile.getBytes())
+                .contentType(mockFile.getContentType())
+                .del_yn("N")
+                .build();
+            productRepository.save(product);
+
+            MultipartFile updatedFile = Mockito.mock(MultipartFile.class);
+
+            Mockito.when(updatedFile.getBytes()).thenThrow(new IOException("파일 읽기 실패"));
+
+            ProductUpdateRequest productUpdateRequest = ProductUpdateRequest.builder()
+                .id(product.getId())
+                .name("상품명 수")
+                .description("상품설정 수정")
+                .price("2000")
+                .category(Category.BREAD)
+                .file(updatedFile)
+                .base64EncodeImage(Base64.getEncoder().encodeToString(mockFile.getBytes()))
+                .contentType(mockFile.getContentType())
+                .build();
+
+            assertThatThrownBy(
+                () -> {
+                    productService.updateProduct(productUpdateRequest);
+                }
+            ).isInstanceOf(Exception.class);
+
+        }
+
     }
 }
