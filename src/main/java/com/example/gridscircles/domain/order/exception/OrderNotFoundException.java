d856ddacca39abccba0699cfcc22fb0319a8add8
pbk2312 <pbk2312@inu.ac.kr>
2025-04-28 09:32:38 +0900

    :recycle: refactor: 필요없는 exception 삭제
    - OrderCancelException 삭제
    - OrderNotFoundException 삭제
    - OrderUpdateException 삭제

--- src/main/java/com/example/gridscircles/domain/order/exception/OrderNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.gridscircles.domain.order.exception;
-
-public class OrderNotFoundException extends RuntimeException {
-
-    public OrderNotFoundException(String message) {
-        super(message);
-    }
-}
